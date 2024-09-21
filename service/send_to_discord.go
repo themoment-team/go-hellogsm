5dd3e35014293ecee56fc734ec814bd6b8e0dcaf
jyeonjyan <jyeonjyan.dev@gmail.com>
2024-09-21 20:48:59 +0900

    default job listener 로써 discord msg 를 보낼 수 있도록 함.

--- service/send_to_discord.go
+++ service/send_to_discord.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"themoment-team/go-hellogsm/internal"
@@ -25,6 +26,8 @@ type Template struct {
 
 type NoticeLevel string
 
+// PingRelayApi
+// 더모먼트팀 RelayAPI 로 Ping 요청을 보낸다.
 func PingRelayApi() error {
 	host := internal.SafeApplicationProperties.API.RelayAPI.URL
 	resp, err := http.Get(host + "/ping")
@@ -35,10 +38,30 @@ func PingRelayApi() error {
 	return nil
 }
 
-func SendDiscordMsg(template Template) error {
+// SendDiscordMsg
+// 더모먼트팀 RelayAPI 를 통해 discord 메시지를 보낸다.
+func SendDiscordMsg(template Template) {
 	host := internal.SafeApplicationProperties.API.RelayAPI.URL
 	apikey := internal.SafeApplicationProperties.API.RelayAPI.Key
 
+	standardization(&template)
+
+	apiClientErr := postMessageToDiscord(template, host, apikey)
+	if apiClientErr != nil {
+		log.Printf("[%s] <- 메시지 전송 처리 중 오류 발생. 상세: [%s]", template.Content, apiClientErr.Error())
+	}
+}
+
+// 디스코드로 전송할 메시지를 규격에 맞게 변경한다.
+func standardization(template *Template) {
+	template.Title = addTitlePrefix(template.Title)
+}
+
+func addTitlePrefix(title string) string {
+	return fmt.Sprintf("[%s] %s", internal.GetActiveProfile().Desc, title)
+}
+
+func postMessageToDiscord(template Template, host string, apikey string) error {
 	marshal, err := json.Marshal(template)
 	check(err)
 	req, err := http.NewRequest("POST", host+"/notice", bytes.NewReader(marshal))
@@ -54,7 +77,6 @@ func SendDiscordMsg(template Template) error {
 		logRequest(marshal)
 		return errors.New("relay-api, discord webhook service unavailable")
 	}
-
 	return nil
 }
 
