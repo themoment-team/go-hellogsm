89a70c70a851b78431c690e5742bee351b39e7dd
jyeonjyan <jyeonjyan.dev@gmail.com>
2024-09-22 19:07:29 +0900

    Job.Step.Processor 에서 error 를 리턴할 수 있도록 함.

--- jobs/step.go
+++ jobs/step.go
@@ -4,5 +4,5 @@ package jobs
 // 그냥 Processor 에 해당하는 함수를 실행시키면 되지만, Step 을 사용해서 객체적으로 구분한다.
 // 추후 Step 에는 Reader, Writer 등의 개념을 부여할 수 있다.
 type Step interface {
-	Processor(*BatchContext)
+	Processor(*BatchContext) error
 }
